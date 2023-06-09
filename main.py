1b206d16198cf72b2e9d237f6a31732b351f6472
すいすい <127290093+05110601@users.noreply.github.com>
2023-06-10 06:36:20 +0900

    Create main.py
    main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+
+mail = "example@example.com"
+password = "PassWord"
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+            "X-Debug-Options":"bugReporterEnabled",
+            "X-Discord-Locale":"ja",
+            "X-Discord-Timezone":"Asia/Tokyo",
+            "Content-Type":"application/json"
+            }
+
+payload = {"login": password, "password": mail, "undelete": "false"}
+
+data = requests.Session().post("https://discord.com/api/v9/auth/login",headers=headers,json=payload)
+
+print(data.status_code)
+print(data.text)
+print(data.encoding)
